6924595cc354d3f3d8bc9e276540dfaa3b1a841c
andika4140 <goku200200>
2022-05-18 16:29:05 +0700

    comment test MainActivity.kt

--- app/src/main/java/com/example/sa_lindungi/UI/home/MainActivity.kt
+++ app/src/main/java/com/example/sa_lindungi/UI/home/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //test push github
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
