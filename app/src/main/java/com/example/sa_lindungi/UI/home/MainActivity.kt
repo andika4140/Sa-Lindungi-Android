4a81a823424b164278c7092bb0be183727c297db
alvinvigo <alvinvigop@gmail.com>
2022-05-20 03:22:32 +0700

    - Tambahan di Donate, Onboard (layout) sama beberapa di activity sama strings. - Tombol next sama linear layourt di onboard kayanya perlu diubah

--- app/src/main/java/com/example/sa_lindungi/UI/home/MainActivity.kt
+++ app/src/main/java/com/example/sa_lindungi/UI/home/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    //test missing
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
